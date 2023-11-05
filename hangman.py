704ee83e52417827ba5f7f81c5bcba2b36a473c2
arjunv075 <arjunv075@gmail.com>
2023-11-05 19:40:13 +0530

    1.code green  test successfully passed with adding remaining modules

--- hangman.py
+++ hangman.py
@@ -25,3 +25,52 @@ def get_mask_word(word, guesses):
         else:
             ret.append("-")
     return "".join(ret)
+
+
+def get_status(secret_word, turns_remaining, guesses):
+    masked_word = get_mask_word(secret_word, guesses)
+    guesses = "".join(guesses)
+    return f"""Secret word : {masked_word}
+Turns remaining : {turns_remaining}
+Guesses so far : {guesses}
+"""
+
+def play_round(secret_word, guesses, guess, turns_remaining):
+    if guess in guesses:
+        return guesses, turns_remaining, "next"
+    guesses.append(guess)
+    if "-" not in get_mask_word(secret_word, guesses): 
+        # Everything has been guessed
+        return guesses, turns_remaining, "game_won"
+
+    if guess not in secret_word:
+        turns_remaining -= 1
+        if turns_remaining == 0:
+            return guesses, turns_remaining, "game_over"
+    return guesses, turns_remaining, "next"
+
+def main():
+    print ("Welcome to Hangman!")
+    print ("-------------------\n\n")
+    secret_word = get_random_word()
+    print (secret_word)
+    turns_remaining = 8
+    guesses = []
+    while True:
+        status = get_status(secret_word, turns_remaining, guesses)
+        print (status)
+        guess = input("Enter your guess ")
+        guesses, turns_remaining, next_action = play_round(secret_word, guesses, guess, turns_remaining)
+        if next_action == "game_over":
+            print (f"You lost. The word is {secret_word}")
+            break
+        if next_action == "game_won":
+            print (f"You won. The word is {secret_word}")
+            break
+
+if __name__ == "__main__":
+    main()
+    
+    
+
+
--- hangmantest.py
+++ hangmantest.py
@@ -40,3 +40,103 @@ def test_mask_word_no_guesses():
     word = "elephant"
     masked_word = hangman.get_mask_word(word, guesses)
     assert masked_word == "--------"
+
+def test_mask_word_single_wrong_guess():
+    guesses = ['x']
+    word = "elephant"
+    masked_word = hangman.get_mask_word(word, guesses)
+    assert masked_word == "--------"
+
+def test_mask_word_single_correct_guess():
+    guesses = ['t']
+    word = "elephant"
+    masked_word = hangman.get_mask_word(word, guesses)
+    assert masked_word == "-------t"
+
+def test_mask_word_two_correct_guesses():
+    guesses = ['p','t']
+    word = "elephant"
+    masked_word = hangman.get_mask_word(word, guesses)
+    assert masked_word == "---p---t"
+
+def test_mask_word_single_guess_multiple_occurrence():
+    guesses = ['e', 'p','t']
+    word = "elephant"
+    masked_word = hangman.get_mask_word(word, guesses)
+    assert masked_word == "e-ep---t"
+
+def test_get_status():
+    secret_word = "policeman"
+    turns_remaining = 75
+    guesses = ['i','c','x']
+    status = hangman.get_status(secret_word,
+                                 turns_remaining,
+                                 guesses,
+                                 )
+    assert status ==  """Secret word : ---ic----
+Turns remaining : 75
+Guesses so far : icx
+"""
+
+def test_play_round_correct_guess():
+    secret_word = "rhino"
+    guesses = []
+    guess = "i"
+    turns_remaining = 8
+    guesses, turns_remaining, next_action = hangman.play_round(secret_word,
+                                                               guesses,
+                                                               guess,
+                                                               turns_remaining)
+    assert guesses == ['i']
+    assert turns_remaining == 8
+    assert next_action == "next"
+
+def test_play_round_wrong_guess_game_not_over():
+    secret_word = "rhino"
+    guesses = ['q']
+    guess = "x"
+    turns_remaining = 8
+    guesses, turns_remaining, next_action = hangman.play_round(secret_word,
+                                                               guesses,
+                                                               guess,
+                                                               turns_remaining)
+    assert guesses == ['q','x']
+    assert turns_remaining == 7
+    assert next_action == "next"
+
+    
+def test_play_round_wrong_guess_game_over():
+    secret_word = "rhino"
+    guesses = ['q']
+    guess = "x"
+    turns_remaining = 1
+    guesses, turns_remaining, next_action = hangman.play_round(secret_word,
+                                                               guesses,
+                                                               guess,
+                                                               turns_remaining)
+    assert next_action == "game_over"
+
+
+def test_play_round_game_win():
+    secret_word = "rhino"
+    guesses = ['r','h', 'i', 'n']
+    guess = "o"
+    turns_remaining = 1
+    guesses, turns_remaining, next_action = hangman.play_round(secret_word,
+                                                               guesses,
+                                                               guess,
+                                                               turns_remaining)
+    assert next_action == "game_won"
+
+def test_play_round_repeated_guess():
+    secret_word = "rhino"
+    guesses = ['r','h']
+    guess = "h"
+    turns_remaining = 5
+    guesses, turns_remaining, next_action = hangman.play_round(secret_word,
+                                                               guesses,
+                                                               guess,
+                                                               turns_remaining)
+    assert guesses == ['r', 'h']
+    assert turns_remaining == 5
+    assert next_action == "next"
